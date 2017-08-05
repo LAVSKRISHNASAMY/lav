3024b4ba72bd0dbaf8012097b080b44c7b5ec8d6
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2017-08-05 16:31:10 +0530

    oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the number %d is even");
+}
+else
+{
+printf("the number %d is odd");
+}
+getch();
+}
