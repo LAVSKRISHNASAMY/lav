8179bd77703304f03bd8356f24927aff8fb8568e
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2017-08-08 17:00:44 +0530

    char.c

---
+++ char.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char c={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','y','z'};
+printff("enter the character:");
+scanf("%c",c);
+if(c1)
+{
+printf("it is a alphabet");
+}
+else
+{
+printf("it ie not a alphabet:");
+}
+}
