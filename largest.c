3a8758bcdd425f8979688b577469ddafb93d78db
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2017-08-08 16:29:59 +0530

    largest.c

---
+++ largest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a=5,b=20,c=2;
+if(a>b && a>c)
+{
+printf("a is greater");
+}
+else if(b>c)
+{
+printf("b is greater");
+}
+else
+{
+printf("c is greater");
+}
+}
+
