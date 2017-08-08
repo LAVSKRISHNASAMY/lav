8509cd91c9724b890ca7dd4a190f6fdddf0d18f6
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2017-08-08 16:53:32 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int sum=0,n,num;
+printf("enter the number");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
+{
+printf("enter the number:");
+scanf("%d",&num);
+sum=sum+num;
+printf("natural natural:",sum);
+}
+}
