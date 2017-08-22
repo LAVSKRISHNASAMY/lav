b2440143237909a0d35a28d8bf70b174529f1942
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2017-08-22 14:41:44 +0530

    char.c

--- char.c
+++ char.c
@@ -3,7 +3,7 @@ void main()
 {
 char c={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','y','z'};
 printff("enter the character:");
-scanf("%c",c);
+scanf("%c",c1);
 if(c1)
 {
 printf("it is a alphabet");
