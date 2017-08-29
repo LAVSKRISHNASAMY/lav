89a0cc8096e597f3ed3b36a634aa7ac488b8ea6b
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2017-08-29 14:44:30 +0530

    palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -6,3 +6,17 @@ printf("enter the number");
 scanf("%d",&n);
 oi=n;
 while(n!=0)
+{
+  r=n%10;
+  ri=ri*10+r;
+  n/=10;
+}
+  if(oi==ri)
+  {
+    printf("it is palindrome",oi);
+  }
+  else
+  {
+    printf("not a palindrome",oi);
+  }
+}
