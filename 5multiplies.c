070260cace34aef0c0d5e44b5b9f9f424d94ae96
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2018-01-10 16:39:48 +0530

    5multiplies.c

---
+++ 5multiplies.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+printf("\n enter the number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf(" \n the first 5multiplies of a given number is :",i,i*5);
+}
+return 0;
+}
