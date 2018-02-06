94c22a32023eb03580581dd42fc6eb291ffadce7
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2018-02-06 21:50:04 +0530

    factorial.c

--- factorial.c
+++ factorial.c
@@ -5,7 +5,7 @@ int i,fact=1,num;
 printf("Enter a number: ");
 scanf("%d",&num);
 for(i=1;i<=num;i++)
-f=f*i;
-printf("Factorial of %d is: %d",num,f);
+fact=fact*i;
+printf("Factorial of %d is: %d",num,fact);
 return 0;
 }
