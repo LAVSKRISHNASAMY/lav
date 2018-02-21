40be49b94bcd01e7b29cd7a8a6b9d74a1ebf604c
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2018-02-21 15:56:36 +0530

    numbtwn2intervals.c

---
+++ numbtwn2intervals.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,l,j;
+scanf("%d %d",&l,&j);
+printf("enter the number");
+scanf("%d",&n);
+if(n>l && n<j)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
