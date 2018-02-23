d9203eaa30eaf71d58150e057a190be7cc1ed767
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2018-02-23 16:04:23 +0530

    odddigits.c

---
+++ odddigits.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,r,i=0,digit;
+printf("enter the integer:");
+scanf("%d",&n);
+printf("%d \n ",n);
+while(n>0)
+{
+digit=n%10;
+n=n/10;
+r=digit%2;
+if(r!=0)
+{
+printf("%d \n",digit);
+}}
+return 0;
+}
+
+
