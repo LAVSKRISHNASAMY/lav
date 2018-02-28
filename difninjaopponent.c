ecd24e26ad16b4c2942845a02fe399cca3be8b4c
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2018-02-28 15:49:56 +0530

    difninjaopponent.c

---
+++ difninjaopponent.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,n,ninja,opponent;
+scanf("%d \n",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d %d",&ninja,&opponent);
+if(opponent>ninja)
+{
+printf("%d\n",opponent-ninja);
+}
+else
+{
+printf("%d\n",ninja-opponent);
+}
+}
+return 0;
+}
