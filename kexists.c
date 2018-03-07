5534596a8a748afec171d900d16114dbc2e2c2b8
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2018-03-07 16:34:03 +0530

    kexists.c

--- kexists.c
+++ kexists.c
@@ -3,11 +3,11 @@ int main()
 {
 int a[10],n,k,flag=0;
 scanf("%d %d",&n,&k);
-for(int i=0;i<5;i++)
+for(int i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
-for(int i=0;i<5;i++)
+for(int i=0;i<n;i++)
 {
 if(a[i]==k)
 {
