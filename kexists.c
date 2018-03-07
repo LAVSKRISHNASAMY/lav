4f092dc221ca188674525424df9e13fc1a94285e
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2018-03-07 16:25:01 +0530

    kexists.c

---
+++ kexists.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a[10],n,k,flag=0;
+scanf("%d %d",&n,&k);
+for(int i=0;i<5;i++)
+{
+scanf("%d",&a[i]);
+}
+for(int i=0;i<5;i++)
+{
+if(a[i]==k)
+{
+printf("yes");
+flag=1;
+break;
+}}
+if(flag==0)
+{
+printf("no");
+}
+return 0;
+}
