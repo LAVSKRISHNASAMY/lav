fd415677d5b4b993f064d90e681e126b816d050d
LAVSKRISHNASAMY <30549550+LAVSKRISHNASAMY@users.noreply.github.com>
2018-03-21 16:05:41 +0530

    lieonsameline.cpp

---
+++ lieonsameline.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+int main()
+{
+int x1,x2,x3,y1,y2,y3;
+cin>>x1>>x2>>x3>>y1>>y2>>y3;
+if(x1 && x2 && x3 >=0)
+{
+if(y1 && y2 && y3>=0)
+{
+cout<<"yes";
+}
+else
+{
+cout<<"no";
+}
+}
+return 0;
+}
